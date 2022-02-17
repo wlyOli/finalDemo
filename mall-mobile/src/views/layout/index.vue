414455770e8d0f93d9046c23dba5282f768790f3
wlyOli <275059542@qq.com>
2022-02-17 10:32:23 +0800

    第一次提交

---
+++ mall-mobile/src/views/layout/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="layout-container">
+   <!-- 子路由的出口-->
+   <router-view />
+   
+   <!-- 底部标签导航栏-->
+	<van-tabbar route v-model="active">
+	  <van-tabbar-item icon="home-o" to="/">首页</van-tabbar-item>
+	  <!-- <van-tabbar-item icon="search" to="/category">分类</van-tabbar-item> -->
+	  <van-tabbar-item icon="shopping-cart-o" to="/shopCar">购物车</van-tabbar-item>
+	  <van-tabbar-item icon="manager-o" to="/my">我的</van-tabbar-item>
+	</van-tabbar>
+  </div>
+</template>
+
+<script>
+	export default{
+		name:'LayoutIndex',
+		data(){
+			return {
+				 active: 0,
+			}
+		}
+	}
+</script>
+
+<style scoped>
+
+</style>
